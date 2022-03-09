9ae44d8154aa25c36bc127fc93ffe3534a403553
Eliot999 <71841438+Eliot999@users.noreply.github.com>
2022-03-09 14:25:08 +0100

    working need for some styling

--- block.py
+++ block.py
@@ -2,14 +2,32 @@ HEIGHT = 20
 
 
 class Block:
-    def __init__(self, HEIGHT, x, w, v):
+    def __init__(self, HEIGHT, x, w, v, m):
         self.x = x
         self.y = HEIGHT - w
         self.w = w
         self.v = v
+        self.m = m
 
-    def collide(block):
-        pass
+    def collide(self, block):
+        return not (self.x + self.w < block.x) or (self.x > block.x + block.w)
 
-    def prep_show():
-        return (this.x, this.y, ...)  # not finished
+    def bounce(self, block):
+        sum_M = self.m + block.m
+        new_V = (self.m-block.m)/sum_M * self.v
+        new_V += (2 * block.m/sum_M) * block.v
+        return new_V
+
+    def hit_wall(self):
+        if self.x <= 0:
+            return True
+        return False
+
+    def reverse(self):
+        self.v *= -1
+
+    def update(self):
+        self.x += self.v
+
+    def prep_show(self):
+        return (self.x, self.y, self.w, self.w)
--- main.py
+++ main.py
@@ -1,24 +1,46 @@
 import pygame
 import sys
-
+from itertools import count
 from block import Block
 
-pygame.init()
 
-SIZE = WIDTH, HEIGHT = 800, 600
+SIZE = WIDTH, HEIGHT = 1200, 800
 BLACK = 0, 0, 0
-WHITE = 255, 255, 255
-screen = pygame.display.set_mode(SIZE)
+WHITE = 200, 200, 200
+DIGITS = 5
+TIME_STEPS = 20000
+counter = count()
 
-block1 = Block(HEIGHT, 100, 20, 0)
-block2 = Block(HEIGHT, 300, 150, -1)
+pygame.init()
+screen = pygame.display.set_mode(SIZE)
+FONT = pygame.font.SysFont("Times New Roman", 24)
+
+block1 = Block(HEIGHT, 200, 20, 0, 1)
+M2 = pow(100, DIGITS-1)
+block2 = Block(HEIGHT, 500, 150, -1/(TIME_STEPS/2), M2)
 
 
 while True:
     screen.fill(BLACK)
-    pygame.draw.rect(screen, WHITE, (395, 0, 10, 10))
+    pygame.draw.rect(screen, WHITE, block1.prep_show())
+    pygame.draw.rect(screen, WHITE, block2.prep_show())
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
+    for i in range(TIME_STEPS):
+        if block1.collide(block2):
+            v1 = block1.bounce(block2)
+            v2 = block2.bounce(block1)
+            block1.v = v1
+            block2.v = v2
+            next(counter)
+        if block1.hit_wall():
+            block1.reverse()
+            next(counter)
+        block1.update()
+        block2.update()
+    numeric_filter = filter(str.isdigit, str(counter))
+    numeric_string = "".join(numeric_filter)
+    font_render = FONT.render(numeric_string, 1, WHITE)
+    screen.blit(font_render, (60, 60))
     pygame.display.flip()
